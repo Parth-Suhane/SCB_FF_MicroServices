952e5ae4d44cc63d7bfbc080761db12ce861132b
unknown <suprajakannaiyan84@gmail.com>
2020-09-01 16:04:02 +0530

    Integrated the loan services into single application

---
+++ loan-management-service/src/main/java/com/cts/scb/loanmgmt/LoanManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.cts.scb.loanmgmt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoanManagementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoanManagementServiceApplication.class, args);
+	}
+
+}
---
+++ loan-management-service/src/test/java/com/cts/scb/loanmgmt/LoanManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cts.scb.loanmgmt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoanManagementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
